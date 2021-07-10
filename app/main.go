e3b2bca3ae30443584b6acd1726375070c607c11
17t2037h <17t2037h@shinshu-u.ac.jp>
2021-07-10 17:42:39 +0900

    made docker setup

--- app/main.go
+++ app/main.go
@@ -1,1 +1,26 @@
+package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+
+	engine.LoadHTMLGlob("./*.html")
+	engine.Static("/js", "./js")
+	engine.Static("/css", "./css")
+	engine.Static("/image", "./image")
+
+	engine.GET("/select", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	err := engine.Run(":8080")
+	if err != nil {
+		log.Fatal("Run: ", err)
+	}
+}
