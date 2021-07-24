58671ba451079957f9c54cd0d254025a2d12690a
kantentenka <yutakimata@gmail.com>
2021-07-24 22:38:24 +0900

    iconの変更

--- app/static/js/result.js
+++ app/static/js/result.js
@@ -66,7 +66,20 @@ var res = new Vue({
         tileLayer.addTo(map);
         
         // マーカー画像の場所を指定する
-        L.marker([i["latitude"], i["longitude"]]).addTo(map)
+        var icon_self_pos = L.icon({
+            iconUrl: '/image/self_pos.png',
+            iconSize:     [32, 32], // アイコンのサイズ
+            iconAnchor:   [16, 52], // マーカーの位置に対応するアイコンの位置
+            popupAnchor:  [0, -55] // ポップアップを開く基準
+        });
+
+        var icon_recommend_pos = L.icon({
+            iconUrl: '/image/recommend_pos.png',
+            iconSize:     [32, 32], // アイコンのサイズ
+            iconAnchor:   [16, 52], // マーカーの位置に対応するアイコンの位置
+            popupAnchor:  [0, -55] // ポップアップを開く基準
+        });
+        L.marker([i["latitude"], i["longitude"]], {icon: icon_self_pos}).addTo(map)
             .bindPopup(`${i["name"]}<br><img  src="/image/${i["id"]}.jpg" height="100">`)
             .openPopup(); 
                     
@@ -79,7 +92,7 @@ var res = new Vue({
                 おすすめポイント<br>
                 ${value["intro"]}
                 `
-            L.marker([value["latitude"], value["longitude"]]).addTo(map)
+            L.marker([value["latitude"], value["longitude"]],{icon: icon_recommend_pos}).addTo(map)
                 .bindPopup(popup);
             });
         }
