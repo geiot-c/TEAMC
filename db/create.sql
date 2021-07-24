98c4a597c7f38b0cc305ae47d80d1db0cc4fea32
17t2037h <17t2037h@shinshu-u.ac.jp>
2021-07-24 13:44:03 +0900

    seeded data

--- db/create.sql
+++ db/create.sql
@@ -20,28 +20,497 @@ CREATE TABLE IF NOT EXISTS recommends (
 );
 
 INSERT INTO recommends (
-  id,
   recommender_id,
   recommended_shop_id,
   intro
 )
 VALUES (
-  1,
   1,
   2,
   "毎日通ってます！"
 );
 
 INSERT INTO recommends (
-  id,
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  1,
+  3,
+  "いい感じです！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  1,
+  4,
+  "大好きです！"
+);
+
+INSERT INTO recommends (
   recommender_id,
   recommended_shop_id,
   intro
 )
 VALUES (
   2,
+  5,
+  "ぜひ行ってみて！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  2,
+  7,
+  "大好きです！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  2,
+  10,
+  "ぜひ行ってみて！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
   3,
   1,
+  "結構いいです。"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  3,
+  6,
+  "毎日通ってます！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  3,
+  8,
+  "ぜひ行ってみて！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  4,
+  2,
+  "結構いいです。"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  4,
+  3,
+  "毎日通ってます！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  4,
+  15,
+  "大好きです！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  5,
+  4,
+  "結構いいです。"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  5,
+  6,
+  "大好きです！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  5,
+  7,
+  "毎日通ってます！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  6,
+  10,
+  "結構いいです。"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  6,
+  11,
+  "大好きです！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  6,
+  14,
+  "すごいです！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  7,
+  9,
+  "毎日通ってます！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  7,
+  13,
+  "結構いいです。"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  7,
+  14,
+  "ぜひ行ってみて！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  8,
+  9,
+  "毎日通ってます！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  8,
+  11,
+  "結構いいです。"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  8,
+  15,
+  "大好きです！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  9,
+  3,
+  "大好きです！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  9,
+  5,
+  "結構いいです。"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  9,
+  10,
+  "ぜひ行ってみて！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  10,
+  2,
+  "毎日通ってます！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  10,
+  6,
+  "大好きです！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  10,
+  12,
+  "結構いいです。"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  11,
+  1,
+  "ぜひ行ってみて！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  11,
+  4,
+  "大好きです！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  11,
+  7,
+  "結構いいです。"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  12,
+  9,
+  "結構いいです。"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  12,
+  13,
+  "ぜひ行ってみて！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  12,
+  14,
+  "毎日通ってます！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  13,
+  2,
+  "結構いいです。"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  13,
+  7,
+  "大好きです！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  13,
+  9,
+  "ぜひ行ってみて！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  14,
+  10,
+  "毎日通ってます！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  14,
+  11,
+  "ぜひ行ってみて！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  14,
+  12,
+  "結構いいです。"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  15,
+  4,
+  "大好きです！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  15,
+  12,
+  "毎日通ってます！"
+);
+
+INSERT INTO recommends (
+  recommender_id,
+  recommended_shop_id,
+  intro
+)
+VALUES (
+  15,
+  13,
   "結構いいです。"
 );
 
@@ -97,12 +566,14 @@ VALUES (
 INSERT INTO shops (
   id,
   shop_name,
+  self_intro,
   latitude,
   longitude
 )
 VALUES (
   4,
   "カフェ エトランジェ ナラッド",
+  "これはテストですがたくさん来てくれるとうれしいです！",
   "34.682309921526944",
   "135.82655925771624"
 );
@@ -111,12 +582,14 @@ VALUES (
 INSERT INTO shops (
   id,
   shop_name,
+  self_intro,
   latitude,
   longitude
 )
 VALUES (
   5,
   "ALL DAY DINING",
+  "これはテストですがたくさん食べてってね！",
   "34.683420861642915",
   "135.82780645642984"
 );
@@ -125,12 +598,14 @@ VALUES (
 INSERT INTO shops (
   id,
   shop_name,
+  self_intro,
   latitude,
   longitude
 )
 VALUES (
   6,
   "中川政七商店　奈良の工芸に触れる体験",
+  "これはテストですが楽しんでいってね！",
   "34.66844085304311",
   "135.83071231303978"
 );
@@ -139,12 +614,14 @@ VALUES (
 INSERT INTO shops (
   id,
   shop_name,
+  self_intro,
   latitude,
   longitude
 )
 VALUES (
   7,
   "今西清兵衛商店",
+  "これはテストですがたくさん見てってください。",
   "34.6771167799442",
   "135.8346802547372"
 );
@@ -153,12 +630,14 @@ VALUES (
 INSERT INTO shops (
   id,
   shop_name,
+  self_intro,
   latitude,
   longitude
 )
 VALUES (
   8,
   "ならまち格子の家",
+  "これはテストですがいろいろ見ていってみてください！",
   "34.67519626978004",
   "135.83075745458194"
 );
@@ -167,12 +646,14 @@ VALUES (
 INSERT INTO shops (
   id,
   shop_name,
+  self_intro,
   latitude,
   longitude
 )
 VALUES (
   9,
   "瑜伽山園地",
+  "これはテストですが中を見回ってみてね！",
   "34.6795394700259",
   "135.83924008867956"
 );
@@ -181,12 +662,14 @@ VALUES (
 INSERT INTO shops (
   id,
   shop_name,
+  self_intro,
   latitude,
   longitude
 )
 VALUES (
   10,
   "寧楽美術館",
+  "これはテストですがたくさん美術品みてってね！",
   "34.68621397298806",
   "135.83719518341763"
 );
@@ -195,12 +678,14 @@ VALUES (
 INSERT INTO shops (
   id,
   shop_name,
+  self_intro,
   latitude,
   longitude
 )
 VALUES (
   11,
   "日本酒とおつまみ　chuin",
+  "これはテストですが美味しいものいっぱいあるよ！",
   "34.67879867636942",
   "135.83066651225275"
 );
@@ -209,12 +694,14 @@ VALUES (
 INSERT INTO shops (
   id,
   shop_name,
+  self_intro,
   latitude,
   longitude
 )
 VALUES (
   12,
   "なら泉勇斎",
+  "これはテストですがたくさん見てみてね！",
   "34.679852836545734",
   "135.82983086566745"
 );
@@ -223,12 +710,14 @@ VALUES (
 INSERT INTO shops (
   id,
   shop_name,
+  self_intro,
   latitude,
   longitude
 )
 VALUES (
   13,
   "樫舎",
+  "これはテストですが楽しんでね！",
   "34.67875598474761",
   "135.8312995000453"
 );
@@ -237,12 +726,14 @@ VALUES (
 INSERT INTO shops (
   id,
   shop_name,
+  self_intro,
   latitude,
   longitude
 )
 VALUES (
   14,
   "SUNNY and MORE",
+  "これはテストですが面白いところですよ！",
   "34.679749291117396",
   "135.8312133815698"
 );
@@ -251,12 +742,14 @@ VALUES (
 INSERT INTO shops (
   id,
   shop_name,
+  self_intro,
   latitude,
   longitude
 )
 VALUES (
   15,
   "ボリクコーヒー",
+  "これはテストですがうちのコーヒー飲んでって！",
   "34.67665865074141",
   "135.83004641964288"
 );
